2e66587aae20e6ac06e805c21568857cd576344f
Alejandro Barrionuevo Rosado <barrionuevorosadoalejandro@gmail.com>
2024-10-22 12:28:54 +0200

    Gatos

---
+++ src/Separation.jsx
@@ -0,0 +1,12 @@
+function Separation() {
+  return (
+    <>
+      <div style={{ textAlign: 'center', margin: '20px 0' }}>
+        <hr style={{ border: '4px solid yellow', width: '80%' }} />
+        <br />
+      </div>
+    </>
+  );
+}
+
+export default Separation;
--- src/main.jsx
+++ src/main.jsx
@@ -2,9 +2,13 @@ import { StrictMode } from 'react'
 import { createRoot } from 'react-dom/client'
 import App from './App.jsx'
 import './index.css'
+import AppCat from './AppCat.jsx'
+import Separation from './Separation.jsx';
 
 createRoot(document.getElementById('root')).render(
   <StrictMode>
     <App />
+    <Separation /> 
+    <AppCat />
   </StrictMode>,
 )
